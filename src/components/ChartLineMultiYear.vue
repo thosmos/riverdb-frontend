ef5f9d914d712edb0c98a3486b8f4966efb405d7
Andreas Groos <andreas.groos1@gmail.com>
2018-08-24 16:38:01 -0700

    moved lineChart into CharLineMultiYear.vue

--- src/components/ChartLineMultiYear.vue
+++ src/components/ChartLineMultiYear.vue
@@ -1,9 +1,94 @@
 <template>
-
+  <highcharts class="chart chart-spacer"
+              :options="chartOptions"></highcharts>
 </template>
 
 <script>
-export default {};
+import { palette } from "../assets/charts.js";
+
+export default {
+  name: "ChartLineMultiYear",
+  props: ["data"],
+  computed: {
+    plotData: function() {
+      console.log("this.data.selectedStation", this.data.selectedStation);
+      if (this.data.loadedStations) {
+        let data = [];
+        this.data.loadedStations.map(s => {
+          let temp = s
+            .setYearRange({ startYear: 2001, endYear: 2017 })
+            .setParam("H2O_Temp")
+            .roundTo(1)
+            .bufferData()
+            .linePlot();
+          data.push({
+            name: s.info.StationName,
+            data: temp.processed.data
+          });
+        });
+        return data;
+      } else {
+        return {};
+      }
+    },
+    chartOptions: function() {
+      return {
+        chart: {
+          type: "spline",
+          zoomType: 'xy"'
+        },
+        title: {
+          text:
+            (this.data.selectedStation &&
+              this.data.selectedStation.info.StationName) ||
+            ""
+        },
+        subtitle: {
+          text: `from ${
+            this.data.selectedStation.meta.totalYearRange.startYear
+          } till ${this.data.selectedStation.meta.totalYearRange.endYear}`
+        },
+        xAxis: {
+          type: "datetime",
+          // dateTimeLabelFormats: {
+          //   // don't display the dummy year
+          //   month: "%e. %b",
+          //   year: "%b"
+          // },
+          title: {
+            text: "Date"
+          }
+        },
+        yAxis: {
+          title: {
+            text: "Temp in C"
+          }
+        },
+        // tooltip: {
+        //   headerFormat: "<b>{series.name}</b><br>",
+        //   pointFormat: "{point.x:%e. %b}: {point.y:.2f} m"
+        // },
+        tooltip: {
+          shared: true
+        },
+        plotOptions: {
+          spline: {
+            marker: {
+              enabled: true,
+              radius: 4
+            }
+          },
+          series: {
+            animation: false
+          }
+        },
+
+        colors: palette,
+        series: this.plotData
+      };
+    }
+  }
+};
 </script>
 
 <style>
--- src/components/ChartWrapper.vue
+++ src/components/ChartWrapper.vue
@@ -1,111 +1,38 @@
 <template>
   <div v-if="data && data.selectedStation"
-       class="ui container">
+       class="ui container fluid">
     <sui-divider></sui-divider>
-    <highcharts class="chart"
-                :options="chartOptions"></highcharts>
-
+    <div class="ui grid">
+      <div class="ui row">
+        <div class="ui column">
+          <chart-line-multi-year :data="data"></chart-line-multi-year>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import graphConfig from "../assets/graphConfig.js";
+import ChartLineMultiYear from "./ChartLineMultiYear";
 
-import { palette } from "../assets/charts.js";
+import { mapState } from "vuex";
 
 export default {
   name: "ChartWrapper",
-
-  data() {
-    return {};
-  },
+  components: { ChartLineMultiYear },
   computed: {
     ...mapState({
       ui: state => state.ui,
       selection: state => state.selection,
       data: state => state.data
-    }),
-    plotData: function() {
-      console.log("this.data.selectedStation", this.data.selectedStation);
-      if (this.data.loadedStations) {
-        let data = [];
-        this.data.loadedStations.map(s => {
-          let temp = s
-            .setYearRange({ startYear: 2001, endYear: 2017 })
-            .setParam("H2O_Temp")
-            .roundTo(1)
-            .bufferData()
-            .linePlot();
-          data.push({
-            name: s.info.StationName,
-            data: temp.processed.data
-          });
-        });
-        return data;
-      } else {
-        return {};
-      }
-    },
-    chartOptions: function() {
-      return {
-        chart: {
-          type: "spline",
-          zoomType: 'xy"'
-        },
-        title: {
-          text:
-            (this.data.selectedStation &&
-              this.data.selectedStation.info.StationName) ||
-            ""
-        },
-        subtitle: {
-          text: `from ${
-            this.data.selectedStation.meta.totalYearRange.startYear
-          } till ${this.data.selectedStation.meta.totalYearRange.endYear}`
-        },
-        xAxis: {
-          type: "datetime",
-          // dateTimeLabelFormats: {
-          //   // don't display the dummy year
-          //   month: "%e. %b",
-          //   year: "%b"
-          // },
-          title: {
-            text: "Date"
-          }
-        },
-        yAxis: {
-          title: {
-            text: "Temp in C"
-          }
-        },
-        // tooltip: {
-        //   headerFormat: "<b>{series.name}</b><br>",
-        //   pointFormat: "{point.x:%e. %b}: {point.y:.2f} m"
-        // },
-        tooltip: {
-          shared: true
-        },
-        plotOptions: {
-          spline: {
-            marker: {
-              enabled: true,
-              radius: 4
-            }
-          },
-          series: {
-            animation: false
-          }
-        },
-
-        colors: palette,
-        series: this.plotData
-      };
-    }
+    })
   }
 };
 </script>
 
 <style>
+.chart-spacer {
+  margin: 10px 30px;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+}
 </style>
