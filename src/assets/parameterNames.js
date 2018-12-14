5413d4e54712c489aa452cd9ff13daf46dac26be
Andreas Groos <andreas.groos1@gmail.com>
2018-12-13 16:10:32 -0800

    Added PO4 and NO3 in /assets/parameterNames.js

--- src/assets/parameterNames.js
+++ src/assets/parameterNames.js
@@ -5,7 +5,9 @@ const names = {
   H2O_Temp: { text: "H2O Temp" },
   H2O_Turbidity: { text: "H2O Turb" },
   H2O_pH: { text: "H2O pH" },
-  H2O_Velocity: { text: "H2O Velocity" }
+  H2O_Velocity: { text: "H2O Velocity" },
+  H2O_PO4: { text: "H2O PO4" },
+  H2O_NO3: { text: "H2O NO3" }
 };
 
 export default names;
